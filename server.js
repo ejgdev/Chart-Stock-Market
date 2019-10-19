c5adaba2feea94ed9ca169750db18345bddca13b
Edward Jimenez <lobox.ed@gmail.com>
2019-10-19 13:23:19 -0500

    fcc: adding chart app

--- server.js
+++ server.js
@@ -1,34 +1,26 @@
 'use strict';
 
-var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
+const express = require('express');
+const routes = require('./app/routes/index.js');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-var app = express();
+const app = express();
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({ extended: true})); // true for parsing application/x-www-form-urlencoded
+app.use(bodyParser.json()); // for parsing application/json
 require('dotenv').load();
-require('./app/config/passport')(passport);
 
-mongoose.connect(process.env.MONGO_URI);
 mongoose.Promise = global.Promise;
+//mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true});
 
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/views', express.static(process.cwd() + '/views'));
 app.use('/common', express.static(process.cwd() + '/app/common'));
 
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-routes(app, passport);
+routes(app);
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
-	console.log('Node.js listening on port ' + port + '...');
+	console.log('Server listening on port ' + port + '...');
 });
